1a02f6b6502a492436af67e5891aac0bd90eeac9
Mahadev1729 <devcampus17@gmail.com>
2025-07-31 16:48:40 +0530

    Fix query string and correct class name in Selenium script for improved element selection

--- localting_single_element.py
+++ localting_single_element.py
@@ -4,10 +4,10 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
-query="laptop"
+query="laptops"
 driver.get(f"https://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords={query}&crid=1IFDC1YF9N37Z&sprefix=laptop%2Caps%2C573")
 
-elem=driver.find_element(By.CLASS_NAME,'puis-card_container')
+elem=driver.find_element(By.CLASS_NAME,"puis-card-container")
 print(elem.text)
 time.sleep(8)
 driver.close()
