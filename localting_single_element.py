711e0410c824f75d7f54bde168b76395c5c6fdba
Mahadev1729 <devcampus17@gmail.com>
2025-07-31 17:17:03 +0530

    Update Selenium script for Amazon search: correct query and class name for improved element selection; add new file for multiple elements

--- localting_single_element.py
+++ localting_single_element.py
@@ -4,12 +4,11 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
-query="laptops"
-driver.get(f"https://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords={query}&crid=1IFDC1YF9N37Z&sprefix=laptop%2Caps%2C573")
-
-elem=driver.find_element(By.CLASS_NAME,"puis-card-container")
+query="Laptops"
+driver.get(f"https://www.amazon.com/s?k={query}&crid=1SMWAVY9D61VS&sprefix=laptop%2Caps%2C760&ref=nb_sb_noss_1")
+elem=driver.find_element(By.CLASS_NAME,"a-section")
 print(elem.text)
-time.sleep(8)
+time.sleep(2)
 driver.close()
 
 
