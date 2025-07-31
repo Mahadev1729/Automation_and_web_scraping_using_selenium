004394d4fe76abc548a2869694dc2e1bf3e06932
Mahadev1729 <devcampus17@gmail.com>
2025-07-31 16:47:00 +0530

    Update element selection in Selenium script to use class name for improved accuracy

--- localting_single_element.py
+++ localting_single_element.py
@@ -7,7 +7,9 @@ driver = webdriver.Chrome()
 query="laptop"
 driver.get(f"https://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords={query}&crid=1IFDC1YF9N37Z&sprefix=laptop%2Caps%2C573")
 
-elem=driver.find_element(By.NAME,'q')
+elem=driver.find_element(By.CLASS_NAME,'puis-card_container')
+print(elem.text)
+time.sleep(8)
 driver.close()
 
 
