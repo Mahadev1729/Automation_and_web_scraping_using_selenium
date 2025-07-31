e3ad1332029caaa3c8d91d413608fd3422ff86e0
Mahadev1729 <devcampus17@gmail.com>
2025-07-31 16:12:54 +0530

    Add .gitignore and initial Selenium script

---
+++ main.py
@@ -0,0 +1,23 @@
+﻿from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.python.org")
+print(driver.title)
+driver.implicitly_wait(5)
+
+search_box = driver.find_element(By.NAME, "q")
+search_button = driver.find_element(By.ID, "submit")
+
+search_box.send_keys("Selenium")
+search_button.click()
+
+time.sleep(2)
+print(driver.title)
+
+first_result = driver.find_element(By.CSS_SELECTOR, ".list-recent-events li a")
+print(first_result.text)
+
+time.sleep(4)
+driver.quit()
